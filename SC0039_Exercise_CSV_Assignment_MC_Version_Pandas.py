f8e0ec432038f5d3d91dff941cfbeff15a92652a
melocelo <melis.celik@gu.se>
2024-09-18 22:10:26 +0200

    Update SC0039_Exercise_CSV_Assignment_MC_Version_Pandas.py

--- SC0039_Exercise_CSV_Assignment_MC_Version_Pandas.py
+++ SC0039_Exercise_CSV_Assignment_MC_Version_Pandas.py
@@ -1,18 +1,16 @@
 import pandas as pd
-def calculate_segment_length(input_file, output_file):
-    # Read the CSV file into a pandas DataFrame
+def calculate_length(input_file, output_file):
+    # Conversion from csv to Pandas
     df = pd.read_csv(input_file)
 
-    # Calculate the segment length and add it as a new column 'seq_length'
+    # To calculate the segment length here we use funtions and we are adding it as a new column 'seq_length'
     df['seq_length'] = df['loc.end'] - df['loc.start']
 
-    # Save the updated DataFrame to a new CSV file
+    # Conversion from Pandas to csv
     df.to_csv(output_file, index=False)
 
 if __name__ == '__main__':
-    # Define the input and output file paths
     input_file = 'brca_cnvs_tcga-1-2-2.csv'
     output_file = 'output.csv'
 
-    # Call the function to process the file and generate the output
-    calculate_segment_length(input_file, output_file)
+    calculate_length(input_file, output_file)
